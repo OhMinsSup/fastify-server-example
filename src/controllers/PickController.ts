e6685d87e534b5019d20e248eace7f4071b4c841
OhMinSeop <mins5190@gmail.com>
2021-11-21 22:00:37 +0900

    ADD: fastify 서버 예시 프로젝트

--- src/controllers/PickController.ts
+++ src/controllers/PickController.ts
@@ -1,4 +1,7 @@
+import { RESULT_CODE } from 'constants/constant'
 import { FastifyRequest, FastifyReply } from 'fastify'
+import { StatusCodes } from 'http-status-codes'
+import Joi from 'joi'
 import PickService from 'services/PickService'
 
 class PickController {
@@ -9,7 +12,41 @@ class PickController {
   }
 
   createPick = async (request: FastifyRequest<any>, reply: FastifyReply) => {
+    const schema = Joi.object().keys({
+      title: Joi.string().required(),
+      description: Joi.string().required(),
+      slug: Joi.string().required(),
+      published: Joi.boolean().required(),
+      userIds: Joi.array().items(Joi.number()),
+      tags: Joi.array().items(Joi.string()),
+      places: Joi.array()
+        .items(
+          Joi.object().keys({
+            name: Joi.string().required(),
+            lat: Joi.number().required(),
+            lng: Joi.number().required(),
+            address_name: Joi.string().required(),
+            category: Joi.string().required(),
+            category_code: Joi.string().required(),
+          })
+        )
+        .required(),
+    })
+
+    const { body } = request as any
+
     try {
+      const validation = schema.validate(body)
+      if (validation.error) {
+        const obj = {
+          ok: false,
+          code: StatusCodes.BAD_REQUEST,
+          resultCode: RESULT_CODE.INVALID,
+          message: validation.error.message,
+        }
+        return reply.status(StatusCodes.BAD_REQUEST).send(obj)
+      }
+
       return reply.status(200).send(true)
     } catch (error) {
       console.error(error)
